f9090448c964ad934c0f01a64e0cf9828499bc96
Oluwatobiloba-O <bamidurooluwatobiloba@gmail.com>
2023-10-06 22:02:28 -0700

    commit 0 to 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	c = 'n';
+	while
+		(c <= 'n') {
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int c;
+	
+	for (c = 0; c < 10; c++;)
+
+	putchar('\n');
+	return (0);
+}
