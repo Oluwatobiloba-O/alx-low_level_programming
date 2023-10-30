c9115624178d6b37938a1b44deece1918980361d
Oluwatobiloba-O <bamidurooluwatobiloba@gmail.com>
2023-10-30 15:12:07 -0700

    commit all

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *create_array - A funtion that creates an array of chars *
+ * @c: create_array
+ * @size: create_array
+ * Return: Always 0
+ */
+char *create_array(unsigned int size, char c)
+{
+char *xter;
+unsigned int x;
+if (size == 0)
+{
+return (NULL);
+}
+
+xter = malloc(size * sizeof(char));
+
+if (xter == NULL)
+{
+return (NULL);
+}
+
+for (x = 0; x < size; x++)
+{
+xter[x] = c;
+}
+
+return (xter);
+}
