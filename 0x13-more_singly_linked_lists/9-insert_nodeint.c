752f7ac3cee304fdc4a0bceaeed557134834e738
Oluwatobiloba Otun <bamidurooluwatobiloba@gmail.com>
2023-11-21 17:18:16 -0500

    nine

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 unsigned int x = 0;
 listint_t *add_node, *recent;
 
-if (!*head)
+if (!head)
 return (NULL);
 
 add_node = malloc(sizeof(listint_t));
