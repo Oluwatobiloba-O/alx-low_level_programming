36e2fdfa21f2ca8ec7ab5141d7cb81e471fa154c
Oluwatobiloba Otun <bamidurooluwatobiloba@gmail.com>
2023-11-21 17:30:33 -0500

    zeroo

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,16 +3,18 @@
 
 /**
 * print_listint - Entry point (print_listint)
+* @h: pointer for list head
 *
-* @h: pointer
 * Return: num
 */
 size_t print_listint(const listint_t *h)
 {
 const listint_t *x;
-size_t num;
+size_t num = 0;
 
 for (x = h, num = 0; x != NULL; num++, x = x->next)
+{
 printf("%d\n", x->n);
+}
 return (num);
 }
