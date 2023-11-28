656b4fb2e5c256675c5e2f6f75c41d58a4c3baa9
Oluwatobiloba-O <bamidurooluwatobiloba@gmail.com>
2023-11-27 16:07:57 -0800

    commit 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,6 +8,34 @@
 #define SE STDERR_FILENO
 
 /**
+* __exit - prints error messages and exits with exit value
+* @error: num is either exit value or file descriptor
+* @s: str is a name, either of the two filenames
+* @fd: file descriptor
+* Return: 0 on success
+**/
+int __exit(int error, char *s, int fd)
+{
+switch (error)
+{
+case 97:
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(error);
+case 98:
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", s);
+exit(error);
+case 99:
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", s);
+exit(error);
+case 100:
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+exit(error);
+default:
+return (0);
+}
+}
+
+/**
 * main - create the copy bash script
 *
 * @ac: argument count
